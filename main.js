bdaff8b6e008c8d4a8cc707b9973bf64733baaa1
deekra <114116356+deekra@users.noreply.github.com>
2022-10-22 20:08:46 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Bullseye! Tutoring</title>
-    <link rel="stylesheet" href="css/c04.css" />
-  </head>
-  <body>
-    <section id="page2">
-      <h1>Bullseye</h1>
-      <img src="images/teacher.png" id="teacher2" alt="" />
-      <section id="blackboard"></section>
-    </section>
-    <script src="js/example.js"></script>
-  </body>
-</html>
+var table = 3;             // Unit of table
+var operator = 'addition'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '';              // Message
+
+if (operator === 'addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
