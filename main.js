2ba8eb4aa44a0a5d7c675e3378589383644cf8e2
deekra <114116356+deekra@users.noreply.github.com>
2022-10-22 20:38:33 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var table = prompt("Please enter a number from 0-10"); // Unit of table
+var operator = 'addition';                             // Type of calculation
+var i = 1;                                             // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';             // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
-}
 
 // Write the message into the page
 var el = document.getElementById('blackboard');
